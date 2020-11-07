a71e40e6551049bb0dab9e51bc8d170e30a909d7
Alexander <43272321+Alex150Rus@users.noreply.github.com>
2020-11-07 14:33:34 +0300

    Turtle moves rectangle when turtle.x == rect.y and turtle.y ==200

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
 
 namespace TurtleGame
 {
@@ -10,6 +11,38 @@ namespace TurtleGame
     {
         static void Main(string[] args)
         {
+            GraphicsWindow.KeyDown += GraphicsWindow_KeyDown;
+            //убрать шлейф за черепашкой
+            Turtle.PenUp();
+
+            GraphicsWindow.BrushColor = "red";
+            var eat = Shapes.AddRectangle(10, 10);
+            Shapes.Move(eat, 200, 200);
+            
+            while (true)
+            {
+                Turtle.Move(10);
+                if (Turtle.X == Shapes.GetLeft(eat) && Turtle.Y == 200)
+                {
+                    Shapes.Move(eat, Turtle.X + 10, 200);
+                }
+            }
+        }
+
+        private static void GraphicsWindow_KeyDown()
+        {
+            if (GraphicsWindow.LastKey == "Up") {
+                Turtle.Angle = 0;
+            } else if(GraphicsWindow.LastKey == "Right")
+            {
+                Turtle.Angle = 90;
+            } else if (GraphicsWindow.LastKey == "Down")
+            {
+                Turtle.Angle = 180;
+            } else if(GraphicsWindow.LastKey == "Left")
+            {
+                Turtle.Angle = 270;
+            }
         }
     }
 }
